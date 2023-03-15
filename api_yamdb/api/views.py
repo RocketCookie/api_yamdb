0c0fff2565d265363159a286f186ddbaba4b4a07
Анастасия Ладыгина <ae.ladygina@gmail.com>
2023-03-15 12:17:43 +0300

    Добавлены модели Reviews, Comments, views и urls для этих моделей

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 
-# Create your views here.
+from .permissions import AuthorOrSuperUserOrAdminOrReadOnly
+from .serializers import CommentSerializer, ReviewSerializer
+from reviews.models import Review
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = (AuthorOrSuperUserOrAdminOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    #def get_title(self):
+    #    return get_object_or_404(Title, id=self.kwargs.get("title_id"))
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title()
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrSuperUserOrAdminOrReadOnly,)
+    pagination_class = LimitOffsetPagination
+
+    def get_review(self):
+        return get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            review=self.get_review()
+        )
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -2,3 +2,54 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name='Автор'
+    )
+    #title = models.ForeignKey(
+    #   Title,
+    #    on_delete=models.CASCADE,
+    #    related_name='reviews',
+    #    verbose_name='Произведение'
+    #)
+    text = models.TextField(verbose_name='Текст')
+    score = models.IntegerField(verbose_name='Оценка')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата добавления'
+    )
+    
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author_title'
+            ),
+        )
+    
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Автор'
+    )
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name='Отзыв'
+    )
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        db_index=True,
+        verbose_name='Дата добавления'
+    )
