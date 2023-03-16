62995b2d00682f55883b41a58c86b5de3cd94b92
Марина Богатырева <marinamor555@yandex.com>
2023-03-16 23:25:28 +0300

    Добавлены api для Title,Genre,Category

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CommentViewSet, ReviewViewSet, TitleViewSet
+from .views import (CommentViewSet, ReviewViewSet,
+                    TitleViewSet, GenreViewSet, CategoryViewSet)
 
 v1_router = DefaultRouter()
-v1_router.register('titles', TitleViewSet)
+v1_router.register(r'titles', TitleViewSet)
+v1_router.register(r'genres', GenreViewSet)
+v1_router.register(r'categories', CategoryViewSet)
 v1_router.register(
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet, basename='reviews'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,32 +1,58 @@
 
-# from rest_framework.pagination import LimitOffsetPagination
-# from rest_framework import filters
-from django.shortcuts import get_object_or_404
-from rest_framework import viewsets
-
-# from rest_framework.permissions import (IsAdminUser)
+from rest_framework import viewsets, mixins
+from reviews.models import Title, Review, Genre, Category
+from .serializers import (TitleSerializer, CommentSerializer,
+                          ReviewSerializer, GenreSerializer,
+                          CategorySerializer)
 from .permissions import AuthorOrSuperUserOrAdminOrReadOnly
-from .serializers import CommentSerializer, ReviewSerializer, TitleSerializer
-from reviews.models import Review, Title
+from rest_framework.pagination import LimitOffsetPagination
+from django.shortcuts import get_object_or_404
+from rest_framework import filters
+
+
+class GenreViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericViewSet):
+    """Вьюсет POST, GET, DELETE методы для Genre сериализатора"""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    pagination_class = LimitOffsetPagination
+
+
+class CategoryViewSet(mixins.ListModelMixin,
+                      mixins.CreateModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    """Вьюсет POST, GET, DELETE методы для Category сериализатора"""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+    pagination_class = LimitOffsetPagination
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     """Вьюсет POST, GET, PATCH, DELETE методы для Title сериализатора"""
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    # permission_classes = (IsAdminUser)
-    # pagination_class = LimitOffsetPagination
-    # filter_backends = (filters.SearchFilter,)
-    # search_fields = ('category__slug', 'genre__slug', 'year')
+    pagination_class = LimitOffsetPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'category__slug', 'genre__slug', 'year')
+    pagination_class = LimitOffsetPagination
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (AuthorOrSuperUserOrAdminOrReadOnly,)
-    # pagination_class = LimitOffsetPagination
+    pagination_class = LimitOffsetPagination
 
-    # def get_title(self):
-    #    return get_object_or_404(Title, id=self.kwargs.get("title_id"))
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get("title_id"))
 
     def get_queryset(self):
         return self.get_title().reviews.all()
@@ -41,7 +67,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (AuthorOrSuperUserOrAdminOrReadOnly,)
-    # pagination_class = LimitOffsetPagination
+    pagination_class = LimitOffsetPagination
 
     def get_review(self):
         return get_object_or_404(Review, pk=self.kwargs.get('review_id'))
