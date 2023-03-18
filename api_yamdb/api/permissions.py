fdd8eec3463b230ac16e7e4124f0045d6177230d
Олег Гуров <guroff.oleg@gmail.com>
2023-03-18 18:53:20 +0300

    Добавлен пермишен только Админ

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -37,3 +37,11 @@ class AdminOrReadOnly(permissions.BasePermission):
             and (
                 request.user.is_superuser
                 or request.user.is_admin))
+
+
+class AdminOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated
+                and (
+                    request.user.is_superuser
+                    or request.user.is_admin))
